3aa0cf5cf7cf4f930dacccfd6f5b035210576614
Matteo Ples <matteo.ples@gmail.com>
2025-11-16 22:34:31 +0100

    Aggiunta OAuth di Google

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,38 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+ 
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('google')->redirect();
+})->name('auth.redirect');
+
+Route::get('/auth/callback', function () {
+    $googleUser = Socialite::driver('google')->user();
+
+    $user = User::updateOrCreate([
+        'google_id' => $googleUser->id,
+    ], [
+        'name' => $googleUser->name,
+        'email' => $googleUser->email,
+        'google_token' => $googleUser->token,
+        'google_refresh_token' => $googleUser->refreshToken,
+    ]);
+
+    Auth::login($user);
+
+    return redirect('/dashboard');
+});
+
+
+Route::middleware('auth')->group(callback: function(): void {
+    Route::get('/dashboard', function () {
+        $user = Auth::user();
+        return "Dashboard di @" . $user->name;
+    });
+});
 
 Route::get('/', function () {
     return view('welcome');
