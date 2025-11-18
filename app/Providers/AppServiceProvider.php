8da967f0bcdadbdfa7c25efd13b57fb6db712f80
Matteo Ples <matteo.ples@gmail.com>
2025-11-18 18:04:25 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\File;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,11 +18,17 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot()
+    public function boot(): void
     {
+        // Crea automaticamente il symlink storage -> public/storage se non esiste
         $link = public_path('storage');
         if (!File::exists($link)) {
-            $this->app->make('files')->link(storage_path('app/public'), $link);
+            try {
+                File::link(storage_path('app/public'), $link);
+            } catch (\Exception $e) {
+                // Se fallisce (per esempio su Render gratuito), ignora
+                \Log::warning('Storage link non creato: ' . $e->getMessage());
+            }
         }
     }
 }
