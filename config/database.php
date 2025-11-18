79964ae177c4fe183c220b2c77a5104009df5e5f
Matteo Ples <matteo.ples@gmail.com>
2025-11-18 19:00:09 +0100

    Bux fig

--- config/database.php
+++ config/database.php
@@ -85,7 +85,6 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
@@ -93,9 +92,8 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'search_schema' => 'public',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
