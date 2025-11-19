52db3921ad6aac424d247e700fecb3c95b050965
Matteo Ples <matteo.ples@gmail.com>
2025-11-19 14:18:36 +0100

    modifica per tailwind.css

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
     plugins: [
+        tailwindcss(),   // <--- DEVE essere attivo
+        
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            origin: 'https://learning-test.onrender.com',
         }),
     ],
-    base: mode === 'production' ? '/' : '', // optional
-    server: {
-        https: true,          // solo per dev locale se vuoi usare https
-        host: true,
-    }
-}));
+});
