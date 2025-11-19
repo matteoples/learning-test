068a23772ac9b82040632e438062b039801e8df5
Matteo Ples <matteo.ples@gmail.com>
2025-11-19 14:23:14 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    base: 'https://learning-test.onrender.com/',
     plugins: [
-        tailwindcss(),   // <--- DEVE essere attivo
-        
+        tailwindcss(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
