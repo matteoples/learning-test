642401711102a2c5127d97e8e4e614cd12c98a19
Matteo Ples <matteo.ples@gmail.com>
2025-11-19 11:21:41 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
-});
+    base: mode === 'production' ? '/' : '', // optional
+    server: {
+        https: true,          // solo per dev locale se vuoi usare https
+        host: true,
+    }
+}));
